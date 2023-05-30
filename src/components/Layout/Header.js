ab29abe7eeff05afb582abb7a3c0abd811eac651
Yash1Hingu <yash23hingu@gmail.com>
2023-05-30 16:19:20 +0530

    ADD -- header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,8 +1,17 @@
+import {Fragment} from 'react';
 import styles from './Header.module.css';
+import headerImage from '../assets/meals.jpg'
 const Header = (props) => {
     return (
-        <header>
-        </header>
+        <Fragment>
+            <header className={styles.header}>
+                <h1>ReactMeal</h1>
+                <button>Cart</button>
+            </header>
+            <div className={styles['main-image']}>
+                <img src={headerImage} alt="The full Table of Food." />
+            </div>
+        </Fragment>
     );
 }
 export default Header;
