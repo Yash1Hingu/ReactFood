9f81c4d8e61dd245e736622bb0b2a8bbfb40fa61
Yash1Hingu <yash23hingu@gmail.com>
2023-06-04 10:29:04 +0530

    ADD -- cartReducer

--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -1,8 +1,34 @@
+import { useReducer } from 'react'; 
 import CartContext from "./cart-context"
+
+const defaultCartItem = {
+    items: [],
+    totalAmount: 0
+}
+const cartReducer = (state,action) => {
+    if(action.type === 'ADD') {
+        const updateCartItems = state.items.concat(action.item);
+        const updateAmount = state.items.totalAmount + action.items.price * action.items.amount;
+
+        return {
+            items: updateCartItems,
+            totalAmount: updateAmount
+        }
+    }
+
+    return defaultCartItem;
+}
+
 const CartContextProvider = props => {
 
-    const addCartContextHandler = props => {};
-    const removeCartContext = props => {};
+    const [cartItems,dispathCartItem] = useReducer(cartReducer,defaultCartItem);
+
+    const addCartContextHandler = (item) => {
+        dispathCartItem({type: 'ADD',item: item})
+    };
+    const removeCartContext = (id) => {
+        dispathCartItem({type: 'REMOVE' , id: id})
+    };
 
     const cartContext = {
         items: [],
