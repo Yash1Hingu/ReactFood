57515b7df07818fc3bd5875a977906159f2a0571
Yash1Hingu <yash23hingu@gmail.com>
2023-06-04 17:29:41 +0530

    ADD -- order btn show logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,14 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Modal from '../UI/Modal'
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
 const Cart = props => {
     const cartCtx = useContext(CartContext);
+    const [cartIsEmpty,setcartIsEmpty] = useState(true);
+
+    if(cartCtx.items.length > 0 && cartIsEmpty) {
+        setcartIsEmpty(false);
+    }
     const cartItems = cartCtx.items.map(item => <li>{item.name}</li>)
     return (
         <Modal onClose={props.onClose}>
@@ -16,7 +21,7 @@ const Cart = props => {
             </span>
             <span className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-                <button className={classes.button}>Order</button>
+                {!cartIsEmpty && <button className={classes.button}>Order</button>}
             </span>
         </Modal>
     )
