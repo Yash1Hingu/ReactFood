1932966826174f70e0fd8a462641d380b0690d0c
Yash1Hingu <yash23hingu@gmail.com>
2023-06-13 22:32:37 +0530

    ADD -- popup animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
-import {useContext} from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cart-context';
 const HeaderCartButton = props => {
     const cartCtx = useContext(CartContext);
-    const numberofCartList = cartCtx.items.reduce((curNumber,item) => {
-        console.log(curNumber + item.amount)
+    const [BtnIshighLighted,setBtnisHighLighted] = useState(false);
+    const numberofCartList = cartCtx.items.reduce((curNumber, item) => {
         return curNumber + item.amount
-    },0);
+    }, 0);
 
-    return <button className={classes.button} onClick={props.onClick}>
+    const { items } = cartCtx;
+    useEffect(() => {
+
+        if(items.length === 0) {
+            return;
+        } else {
+            setBtnisHighLighted(true);
+        }
+
+        const timer = setTimeout(() => {
+            setBtnisHighLighted(false)
+        },300);
+
+        return () => {
+            clearInterval(timer);
+        }
+    },[items])
+
+    const cartBtnclass = `${classes.button} ${BtnIshighLighted ? classes.bump : ''}`;
+
+    return <button className={cartBtnclass} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
